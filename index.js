1f4be4630b5e4876eb5c593832cf90bde392f026
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-12 12:33:35 -0600

    data renders successfully! :D

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 const questions = require('./lib/questions');
+const fs = require('fs');
+const generateHTML = require('./src/generateHtml');
+
+internCards = [];
+managerCards = [];
+engineerCards = [];
 
 var choice = function() {
     questions.promptAction()
@@ -8,18 +14,28 @@ var choice = function() {
                 questions.promptAddEngineer()
                     .then(engineerData => {
                         if(engineerData.engineer_name && engineerData.engineer_id && engineerData.engineer_email && engineerData.engineer_github){
+                            console.log(engineerData);
+                            engineerCards.push(engineerData);
                             choice();
                         }
                     })
             } else if (promptData.options === 'Add a new intern') {
                 questions.promptAddIntern()
                     .then(internData => {
-                        if(internData.intern_name && internData.intern_id && internData.intern_email && internData.intern_school){
+                        if(internData.intern_name && internData.intern_id && internData.intern_email && internData.intern_school) {
+                            console.log(internData);
+                            internCards.push(internData);
                             choice();
                         }
                     })
             } else if (promptData.options === 'Finish building my team') {
-                console.log('program ended');
+                console.log('finished')
+                const pageHTML = generateHTML(managerCards, engineerCards, internCards);
+
+                fs.writeFile('index.html', pageHTML, err => {
+                    if (err) throw err;
+                    console.log("file has been generated")
+                })
             }
         })
 }
@@ -27,9 +43,8 @@ var choice = function() {
 questions.promptAddManager()
     .then(inputData => {
         if (inputData.manager_name && inputData.manager_id && inputData.manager_email && inputData.manager_office) {
+            console.log(inputData);
+            managerCards.push(inputData);
             choice();
         }    
     })
-
-
-
---
+++ src/generateHtml.js
@@ -0,0 +1,67 @@
+
+
+const generateHTML = function(managerData, engineerData, internData) {
+
+var formattedManager = [];
+var formattedEngineer = [];
+var formattedIntern = [];
+
+for (let i = 0; i < managerData.length; i++) {
+    console.log(managerData)
+    cardDiv = `<div class="card">
+        <h2>${managerData[i].manager_name}</h2>
+        <p>ID: ${managerData[i].manager_id}</p>
+        <p>Email Address: ${managerData[i].manager_email}</p>
+        <p>Office Number: ${managerData[i].manager_office}</p>
+    </div>`
+    formattedManager.push(cardDiv);
+}
+
+for (let i = 0; i < engineerData.length; i++) {
+    console.log(engineerData)
+    cardDiv = `<div class="card">
+        <h2>${engineerData[i].engineer_name}</h2>
+        <p>ID: ${engineerData[i].engineer_id}</p>
+        <p>Email Address: ${engineerData[i].engineer_email}</p>
+        <a href="github.com/${engineerData[i].engineer_github}">Github: ${engineerData[i].engineer_github}</a>
+    </div>`
+    formattedEngineer.push(cardDiv);
+}
+
+for (let i = 0; i < internData.length; i++) {
+    console.log(internData)
+    cardDiv = `<div class="card">
+        <h2>${internData[i].intern_name}</h2>
+        <p>ID: ${internData[i].intern_id}</p>
+        <p>Email Address: ${internData[i].intern_email}</p>
+        <p>School: ${internData[i].intern_school}</p>
+    </div>`
+    formattedIntern.push(cardDiv);
+}
+
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programming Team</title>
+</head>
+<body>
+    <header>
+        <h1>Programming Team</h1>
+    </header>
+    <main id="content">
+        ${formattedManager.join("")}
+        ${formattedEngineer.join("")}
+        ${formattedIntern.join("")}
+    </main>
+    <script src="../src/scripts.js"></script>
+</body>
+</html>
+`
+}
+
+
+module.exports = generateHTML;
